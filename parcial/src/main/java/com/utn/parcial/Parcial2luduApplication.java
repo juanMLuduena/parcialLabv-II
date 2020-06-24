f1b3d69a9362915dd15c6e796b8b7da500d31418
juanMluduena <juanmluduena1@gmail.com>
2020-06-24 18:25:27 -0300

    Spring Configurado + gitignore

---
+++ parcial/src/main/java/com/utn/parcial/Parcial2luduApplication.java
@@ -0,0 +1,13 @@
+package com.utn.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2luduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2luduApplication.class, args);
+	}
+
+}
---
+++ parcial/src/test/java/com/utn/parcial/Parcial2luduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2luduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
